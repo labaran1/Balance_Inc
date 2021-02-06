0e68d4ee1cfa3abbeb9c2b0312f56af0813f519d
labaran1 <adlabaran@gmail.com>
2021-02-06 14:50:20 -0500

    setup file structure

---
+++ src/Component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBox from './SearchBox';
+import './Home.css'
+export default function Home() {
+    return (
+        <center>
+            <div className="searchContainer">
+                <h1>Todo  App</h1>
+
+                <SearchBox/>
+            
+            </div>
+
+
+
+        </center>
+    )
+}
---
+++ src/Component/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SearchBox.css'
+
+export default function SearchBox() {
+    return (
+        <div>
+            SearchBox
+        </div>
+    )
+}
