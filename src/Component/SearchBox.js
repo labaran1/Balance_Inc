c500dde565fc3f401b44bb02c04a73eb1d9c7e27
labaran1 <adlabaran@gmail.com>
2021-02-06 21:39:54 -0500

    update search box

--- src/Component/SearchBox.js
+++ src/Component/SearchBox.js
@@ -1,10 +1,17 @@
-import React,{useState} from 'react'
+import React,{useContext, useState} from 'react'
+import AppContext from '../Context/AppContext/appContext'
 import './SearchBox.css'
 
 export default function SearchBox() {
     const [searchtext, setSearchtext] = useState("")
+const appContext = useContext(AppContext)
+    const sub=(e)=>{
+        e.preventDefault()
+
+appContext.addTodo(searchtext)
+    }
     return (
-        <form>
+        <form onSubmit={sub}>
             <input type="text" name="search" id="name" placeholder="add new todo" value={searchtext} onChange={(e)=>{
 setSearchtext(e.target.value)
             }}/>
