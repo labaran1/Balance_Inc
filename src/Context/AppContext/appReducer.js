625cc8a73fd27c678d7f2ec015130e5a99a2da2d
labaran1 <adlabaran@gmail.com>
2021-02-06 21:37:45 -0500

    update  state reducer

--- src/Context/AppContext/appReducer.js
+++ src/Context/AppContext/appReducer.js
@@ -1,8 +1,15 @@
 import {GET_TODOS,ADD_TODO,DELETE_TODO} from '../types'
+import AppContext from './appContext';
 
 
 const  appReducer = (state, action)=> {
 switch (action.type) {
+    case ADD_TODO:{
+        return {
+            ...state,
+          todos:[...state.todos, action.payload.value]
+        }
+    }
    
         
     
