9bdda162bfb405e74b0b97a26da8182426c463f0
labaran1 <adlabaran@gmail.com>
2021-02-07 22:03:52 -0500

    new todo shows on top

--- src/Context/AppContext/appReducer.js
+++ src/Context/AppContext/appReducer.js
@@ -7,9 +7,15 @@ switch (action.type) {
     case ADD_TODO:{
         return {
             ...state,
-          todos:[...state.todos, action.payload.value]
+          todos:[ action.payload.value, ...state.todos]
         }
     }
+//     case DELETE_TODO:{
+//         return {
+// ...state,
+// todos:[...state.todos].splice(action.payload.val,1)
+//         }
+//     }
    
         
     
